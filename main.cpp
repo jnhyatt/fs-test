060fe9b686cceaa8cb8186262d3445f0f563d45c
jnhyatt <joshua.n.hyatt@gmail.com>
2020-03-04 06:34:14 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main() {
+    std::filesystem::path path;
+    std::istringstream ss("file text.txt");
+    std::string directive;
+
+    ss >> directive >> path;
+    assert(directive == "file");
+
+    std::ifstream fileIn(path);
+    assert(fileIn);
+
+    std::cout << fileIn.rdbuf();
+
+    return 0;
+}
